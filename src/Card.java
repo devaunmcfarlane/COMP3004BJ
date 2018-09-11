3ec447a1f43f9a670d8a6d54f00a075e89baf1fb
devaunmcfarlane <devaunmcfarlane@gmail.com>
2018-09-11 13:58:23 -0400

    Creation of Repository
    Includes Barebones Card Class

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+enum Suite {Hearts, Diamonds, Spades, Clubs}
+enum CardValue {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+public class Card {
+    int pointValue;
+    Suite suite;
+    CardValue cardValue;
+    Card(String suit, String value) {
+        this.suite = suite.Hearts;
+    }
+    }
+
+}
+
