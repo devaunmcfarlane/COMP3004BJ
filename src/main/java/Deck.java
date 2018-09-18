2df7a154ea831ee5ef9196a3cd8d9890542cfae8
DeVaun <devaunmcfarlane@gmail.com>
2018-09-18 13:56:32 -0400

    Deck class work

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,10 +1,124 @@
 import java.util.ArrayList;
 
 public class Deck {
-    ArrayList<Card> cards = new ArrayList();
+   private  ArrayList<Card> cards = new ArrayList();
 
+   Deck(){
+       //Initializing Deck
+       Card HA = new Card("Hearts", "Ace");
+       cards.add(HA);
+        Card H2 = new Card("Hearts", "2");
+        cards.add(H2);
+        Card H3 = new Card("Hearts", "3");
+        cards.add(H3);
+        Card H4 = new Card("Hearts", "4");
+        cards.add(H4);
+        Card H5 = new Card("Hearts", "5");
+        cards.add(H5);
+        Card H6 = new Card("Hearts", "6");
+        cards.add(H6);
+        Card H7 = new Card("Hearts", "7");
+        cards.add(H7);
+        Card H8 = new Card("Hearts", "8");
+        cards.add(H8);
+        Card H9 = new Card("Hearts", "9");
+        cards.add(H9);
+        Card H0 = new Card("Hearts", "10");
+        cards.add(H0);
+        Card HJ = new Card("Hearts", "Jack");
+        cards.add(HJ);
+        Card HQ = new Card("Hearts", "Queen");
+        cards.add(HQ);
+        Card HK = new Card("Hearts", "Ace");
+        cards.add(HK);
 
+        Card DA = new Card("Diamonds", "Ace");
+        cards.add(DA);
+        Card D2 = new Card("Diamonds", "2");
+        cards.add(D2);
+        Card D3 = new Card("Diamonds", "3");
+        cards.add(D3);
+        Card D4 = new Card("Diamonds", "4");
+        cards.add(D4);
+        Card D5 = new Card("Diamonds", "5");
+        cards.add(D5);
+        Card D6 = new Card("Diamonds", "6");
+        cards.add(D6);
+        Card D7 = new Card("Diamonds", "7");
+        cards.add(D7);
+        Card D8 = new Card("Diamonds", "8");
+        cards.add(D8);
+        Card D9 = new Card("Diamonds", "9");
+        cards.add(D9);
+        Card D0 = new Card("Diamonds", "10");
+        cards.add(D0);
+        Card DJ = new Card("Diamonds", "Jack");
+        cards.add(DJ);
+        Card DQ = new Card("Diamonds", "Queen");
+        cards.add(DQ);
+        Card DK = new Card("Diamonds", "Ace");
+        cards.add(DK);
+
+        Card SA = new Card("Spades", "Ace");
+        cards.add(SA);
+        Card S2 = new Card("Spades", "2");
+        cards.add(S2);
+        Card S3 = new Card("Spades", "3");
+        cards.add(S3);
+        Card S4 = new Card("Spades", "4");
+        cards.add(S4);
+        Card S5 = new Card("Spades", "5");
+        cards.add(S5);
+        Card S6 = new Card("Spades", "6");
+        cards.add(S6);
+        Card S7 = new Card("Spades", "7");
+        cards.add(S7);
+        Card S8 = new Card("Spades", "8");
+        cards.add(S8);
+        Card S9 = new Card("Spades", "9");
+        cards.add(S9);
+        Card S0 = new Card("Spades", "10");
+        cards.add(S0);
+        Card SJ = new Card("Spades", "Jack");
+        cards.add(SJ);
+        Card SQ = new Card("Spades", "Queen");
+        cards.add(SQ);
+        Card SK = new Card("Spades", "Ace");
+        cards.add(SK);
+
+        Card CA = new Card("Clubs", "Ace");
+        cards.add(CA);
+        Card C2 = new Card("Clubs", "2");
+        cards.add(C2);
+        Card C3 = new Card("Clubs", "3");
+        cards.add(C3);
+        Card C4 = new Card("Clubs", "4");
+        cards.add(C4);
+        Card C5 = new Card("Clubs", "5");
+        cards.add(C5);
+        Card C6 = new Card("Clubs", "6");
+        cards.add(C6);
+        Card C7 = new Card("Clubs", "7");
+        cards.add(C7);
+        Card C8 = new Card("Clubs", "8");
+        cards.add(C8);
+        Card C9 = new Card("Clubs", "9");
+        cards.add(C9);
+        Card C0 = new Card("Clubs", "10");
+        cards.add(C0);
+        Card CJ = new Card("Clubs", "Jack");
+        cards.add(CJ);
+        Card CQ = new Card("Clubs", "Queen");
+        cards.add(CQ);
+        Card CK = new Card("Clubs", "Ace");
+        cards.add(CK);
+
+    }
     public void shuffle(){
+
+    }
+
+    public Card getAtIndex(int i){
 
     }
 
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -2,12 +2,10 @@ import junit.framework.TestCase;
 
 public class DeckTest extends TestCase {
 
-    public void testInit(){
+    public void testGetAtIndex(){
         Deck testDeck = new Deck;
-        testDeck.init();
-        Card top = testDeck.cards.get(0);
-        assertEquals(card1, top);
     }
+
     public void testShuffle(){
 
     }
